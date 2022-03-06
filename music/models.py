530123a00b285466348d7cce9e6b1a1d502ba2c9
Amy Gori <amydgori@gmail.com>
2022-03-06 15:59:46 -0500

    Add Album and Artist models and set up admin

--- music/models.py
+++ music/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Album(BaseModel):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(
+        "Artist", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    release_date = models.DateField(blank=True, null=True)
+
+    def __repr__(self):
+        return f"<Album {self.title} pk={self.pk} >"
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(BaseModel):
+    INDIVIDUAL = "IND"
+    GROUP = "GRP"
+
+    ARTIST_TYPE_CHOICES = ((INDIVIDUAL, "individual"), (GROUP, "group"))
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=3, choices=ARTIST_TYPE_CHOICES)
+
+    def __repr__(self):
+        return f"<Artist {self.name} pk={self.pk}>"
+
+    def __str__(self):
+        return self.name
